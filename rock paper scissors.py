03dd7c5adf876b7bf48f659ffbe14afd3f82d0da
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2021-03-23 20:57:36 -0500

    Added helper method to determine if player wins

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -9,3 +9,6 @@ def play():
         return 'tie'
 
 def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+        return True
